c6c2cd31ae9096bd32bbd37dd295e2818f61ee27
Gaetan Lavirotte <gaetan.lavirotte@etu.univ-grenoble.fr>
2021-12-11 18:59:57 +0100

    Ajout de commentaires

--- README.md
+++ README.md
@@ -5,10 +5,11 @@
 - Pour compiler: make
 - Pour exécuter le serveur: ./serverThread [n°port]
 - Pour exécuter le serveur: ./serverThread [@IP] [n°port] [pseudo](on prendra comme @IP: 127.0.0.1 si on souhaite tester le serveur en local)
-- Pour afficher la liste des utilisateurs connectés sur le serveur: "liste"
-- Pour se connecter à un utilisateur et lui envoyer des messages: "envoi [pseudo]"
-- Pour sortir de la discussion en cours: "quit"
-- Pour quitter le serveur: "exit"
+- Pour afficher la liste des utilisateurs connectés sur le serveur: "Liste"
+- Pour se connecter à un utilisateur et lui envoyer des messages: "Envoi [pseudo]"
+- Pour sortir de la discussion en cours: "Quit"
+- Pour quitter le serveur: "Exit"
 
 ## Ce que nous n'avons pas fait:
 - Résolution du nom de serveur (exemple: mandelbrot) en adresse IP
+- Nous n'avons pas réussi à mettre en place la commande "Envoi [pseudo] [message]" (le message s'affiche seulement quand le client récepteur  
--- serverThread.c
+++ serverThread.c
@@ -23,15 +23,15 @@ void* socketChat(void *arg){
  	int result = clientfd;
  	char a = clientfd + '0';				
 
- 	retval = write(clientfd, &a, 1);
+ 	retval = write(clientfd, &a, 1);			// Envoi de son id au client 
  	if(retval < 0){
  		perror("\nError in sending client id");
  		exit(1);
  	}
 
 	bzero(buffer, BUF_SIZE);
-	retval = read(clientfd, buffer, 50);
-	strcpy(pseudo, buffer);
+	retval = read(clientfd, buffer, 50);	// Réception du pseudo
+	strcpy(pseudo, buffer);					// copie du pseudo depuis le buffer
 	printf("%s\n", pseudo);
  	if(retval < 0){
  		perror("\nError in receiving pseudo");
@@ -39,66 +39,54 @@ void* socketChat(void *arg){
 	}
 
 	int id = result;
-	setPseudo(id, pseudo);
+	setPseudo(id, pseudo);					// Ecriture du pseudo dans la LinkedList
 
-	while(1){
+	while(1){				
 		bzero(buffer, BUF_SIZE);
-		retval = read(clientfd, buffer, BUF_SIZE);
+		retval = read(clientfd, buffer, BUF_SIZE);	// Lecture du message et écriture dans clientfd
 
 		if(retval < 0){
 			perror("\nReading Error\n");
 			break;
 		}
-		printf("Client %d (%s): %s", clientfd, getPseudoFromID(clientfd), buffer);
+		printf("Client %d (%s): %s", clientfd, getPseudoFromID(clientfd), buffer);	// Affichage des infos dans le terminal du serveur
 		if(strncmp(buffer, "Exit", 4) == 0 || strlen(buffer) < 1){
 			result = clientfd;
 			break;
 		}
-		else if(strncmp(buffer,"Envoi", 5)==0){
-			char * tmp = extractPseudo(buffer + 6);
-			targetclient = getIDFromPseudo(tmp);
-			char * firstMessage = malloc(sizeof(char)* 500);
-			firstMessage = extractFirstMessage(buffer + 6);
-			if(isValid(targetclient, clientfd) == 1){
+		else if(strncmp(buffer,"Envoi", 5)==0){				// Si le message contient l'instruction Envoi
+			char * tmp = extractPseudo(buffer + 6);			// On récupère le pseudo du destinataire
+			targetclient = getIDFromPseudo(tmp);			// On récupère l'id du destinataire
+			if(isValid(targetclient, clientfd) == 1){		// Si le client est valide (toujours connecté)
 				printf("[+] Client %s est connecté au client %s\n", getPseudoFromID(clientfd), getPseudoFromID(targetclient));
 
-				// bzero(buffer, BUF_SIZE);							// Problème ! le message arrive plus tard
-				// result = targetclient;
-				// strcpy(buffer, firstMessage);
-				// retval = write(result, buffer, strlen(buffer));
-				// if(retval < 0){
-				// 	perror("Writing Error");
-				// 	break;
-				// }
-
-				bzero(buffer, BUF_SIZE);
-				strcpy(buffer, "Connexion réussie !\n");
+				bzero(buffer, BUF_SIZE);					// Remise à zéro du buffer
+				strcpy(buffer, "Connexion réussie !\n");	// Copie du message dans le buffer
 				result = clientfd;
-				fflush(stdin);
-				free(firstMessage);
+				fflush(stdin);								// On vide le buffer de l'entrée standard
 			}
 			else{
-				printf("[+] %s ne peut pas se connecter à %s\n", getPseudoFromID(clientfd), getPseudoFromID(targetclient));
+				printf("[+] %s ne peut pas se connecter à %s\n", getPseudoFromID(clientfd), getPseudoFromID(targetclient));	// Infos connexion impossible
 				bzero(buffer, BUF_SIZE);
-				strcpy(buffer, "Client invalide\n");
+				strcpy(buffer, "Client invalide\n");	// Envoi du message d'erreur au client 
 				targetclient = -1;
 			}
 		}
-		else if(strncmp(buffer,"Liste",5)==0){
+		else if(strncmp(buffer,"Liste",5)==0){		// Si le message contient l'instruction Liste
 			bzero(buffer, BUF_SIZE);
-			strcpy(buffer, print(clientfd));
+			strcpy(buffer, print(clientfd));		// Affichage de tous les clients connectés via la LinkedList
 			printf("\n%s", print());
 			result = clientfd;
 			fflush(stdin);
 		}
-		else if(strncmp(buffer,"Quit",4)==0){
+		else if(strncmp(buffer,"Quit",4)==0){		// Si le message contient l'instruction Quit
 			printf("\n[+] %s s'est déconnecté de %s\n", getPseudoFromID(clientfd), getPseudoFromID(targetclient));
 			bzero(buffer, BUF_SIZE);
 			result = targetclient;
 
-			strcpy(buffer, strcat(getPseudoFromID(clientfd), " s'est déconnecté !\n"));
+			strcpy(buffer, strcat(getPseudoFromID(clientfd), " s'est déconnecté !\n"));	// Copie du message d'information de déconnexion dans le buffer
 
-			retval = write(result, buffer, strlen(buffer));
+			retval = write(result, buffer, strlen(buffer)); // Envoi de l'information de déconnexion au client 
 			if(retval < 0){
 				perror("Writing Error");
 				break;
@@ -126,7 +114,7 @@ void* socketChat(void *arg){
 				strcat(buffer, temp);
 
 				printf("\n[+] Envoi d'un message de %s à %s\n", getPseudoFromID(clientfd), getPseudoFromID(result));
-				// Envoi du message au targetClient
+				// Envoi du message au client cible
 				bzero(temp, BUF_SIZE);
 				strcpy(temp, "Envoi du message à ");
 				strcat(temp, getPseudoFromID(targetclient));
@@ -140,18 +128,18 @@ void* socketChat(void *arg){
 		}
 
 		if(result == -1){
-			result = clientfd;
+			result = clientfd;	// Le message sera renvoyé à l'émetteur
 		}
 
 		fflush(stdin);
-		retval = write(result, buffer, strlen(buffer));
+		retval = write(result, buffer, strlen(buffer));	// Ecriture du message sur la socket
 		if(retval < 0){
 			perror("Writing Error");
 			break;
 		}
 	}
-	pthread_mutex_lock(&lock);
-	findandremove(clientfd);
+	pthread_mutex_lock(&lock);		// Blocage des accès concurents pour la suppression d'un client dans la LinkedList
+	findandremove(clientfd);		// Suppresion du client
 	pthread_mutex_unlock(&lock);
 	retval = write(result, buffer, strlen(buffer)); //send exit message to client
 	if(retval < 0){
@@ -168,10 +156,12 @@ int main(int argc, char *argv[]){
 		exit(1);
 	}
 
+	// Configuration
+
 	int sockfd, clientfd, portno, retval;
 	struct sockaddr_in server_addr, client_addr;
 	socklen_t clientlen;
-	portno = atoi(argv[1]);
+	portno = atoi(argv[1]);		// Ecriture du numéro de port d'écoute
 
 // 1) Socket function
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -179,20 +169,19 @@ int main(int argc, char *argv[]){
 		perror("Socket creation Error");
 		exit(1);
 	}
-	bzero((char *)&server_addr, sizeof(server_addr)); // fills with zero
-
+	bzero((char *)&server_addr, sizeof(server_addr)); // Remplissage avec des zeros
 	// set the values in structure
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // any local address
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_addr.sin_port = htons(portno);
 
-// 2) Bind function
+// 2) Bind
 	if(bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
 		perror("Binding Error");
 		exit(1);
 	}
 
-// 3) Listen function
+// 3) Listen
 	if(listen(sockfd, NO_OF_CLIENTS) == 0)
 		printf("Listening on port: %d", portno);
 	else
@@ -201,7 +190,7 @@ int main(int argc, char *argv[]){
 	pthread_t pid[NO_OF_CLIENTS];
 	int i = 0;
 	while(1 && i < NO_OF_CLIENTS){
-	// 4) Accept function
+	// 4) Accept
 		clientlen = sizeof(client_addr);
 		clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &clientlen);
 		if(clientfd < 0){
@@ -214,11 +203,11 @@ int main(int argc, char *argv[]){
 			pthread_mutex_unlock(&lock); 
 			printf("\n[+] Client %d s'est connecté au serveur\n", clientfd);
 		}
-	// 5) Threading
+	// 5) Parallélisme pour gérer les différents chat
 		pthread_create(&pid[i],NULL,&socketChat,&clientfd);
 		i++;
 	}
-	close(sockfd);
+	close(sockfd);	// Fermeture de la socket
 
 	return 0;
 }
